4357985b3a966e93b3f2abebe3a7100ac3e31747
rubenzinho <samuelrubenscontato@gmail.com>
2021-08-08 20:35:31 +0000

    merge

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { TextField, ImageList } from '@material-ui/core'
-import Link from 'next/link';
-
-export const getStaticProps = async() => {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=200/');
-    const data = await res.json();
-
-    return{
-        props: {pokemons: data}
-    }
-}
-
-const Pokedex = ({pokemons}) => {
-    return(
-        <div>
-            <TextField id="Pokémon" label="Pokémon" variant="outlined" autoFocus={true} style={{marginTop:"80px"}}/>
-
-        {pokemons.results.map(pokemon =>(
-            <div key = {pokemon.name}>
-                <a>
-                    <Link href = {`/pokemon/${pokemon.name}`}>
-                      <a>{pokemon.name}</a>
-                    </Link>
-                </a>
-            </div>
-        ))}
-        </div>
-    );
-};
-
-export default Pokedex
-=======
 import useFetchPokemon from './api/useRequest'
 import Pokemon from '../components/pokemonCard'
 import classe from '../styles/Header.module.css'
@@ -65,4 +31,3 @@ export default function IndexPage() {
     </main>
   )
 }
->>>>>>> rubenzola
