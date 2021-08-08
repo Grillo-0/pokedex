36dfd2ee50e695e31b9c0341a4ef84b7822fe16a
Arthur Grillo <arthur.grillo@usp.br>
2021-08-08 20:46:18 -0300

    Adding the pokemon page

---
+++ pages/[name].js
@@ -0,0 +1,36 @@
+const Pokemon = ({pokemon}) => {
+    return(
+        <div>
+        <h1>pagina do pokemon {pokemon.name} com o id {pokemon.id}</h1>
+        <img src={pokemon.sprites.front_default} alt="Pokemon front sprite" layout='fill' />
+        <img src={pokemon.sprites.back_default} alt="Pokemon back sprite" layout='fill' />
+        </div>
+    )
+};
+
+export async function getStaticPaths() {
+    const countRes = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    const countData = await countRes.json()
+    const count = countData.count
+
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${count}/`);
+    const data = await res.json();
+    
+    const paths = data.results.map((pokemon) => ({
+        params: {name : pokemon.name},
+    }))
+
+    return{paths,fallback: false}
+}
+
+export async function getStaticProps({params}){
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`)
+    const pokemon = await res.json()
+
+    return{
+        props: {pokemon}
+    }
+}
+
+
+export default Pokemon
