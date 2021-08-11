3344e80734881a9ca4452f2b6f960df1d61653e9
Arthur Grillo <arthur.grillo@usp.br>
2021-08-11 14:33:40 -0300

    Adding the Header and name tag on pokemon page

--- pages/[name].js
+++ pages/[name].js
@@ -1,51 +1,55 @@
+import Header from '../components/header'
+
 const Pokemon = ({pokemon}) => {
     return(
-        <div className = "pokemonDescription">
-        <h1>#{pokemon.id}: {pokemon.name}</h1>
-        <img src={pokemon.sprites.front_default} alt="Pokemon front sprite" layout='fill' />
-        <img src={pokemon.sprites.back_default} alt="Pokemon back sprite" layout='fill' />
+        <main className="App">
+            <Header/>
+            <div className="PokeNameTag">
+                <div  className="NameType">
+                    <h1>{pokemon.name[0].toUpperCase()+pokemon.name.substring(1)}</h1>
+                    {pokemon.types.map( (type, index) => {
+                        return (<span>{index ? ', ' : ''} {type.type.name}</span>)
+                    })}
+                </div>
+                <h1 id="pokeid">#{pokemon.id}</h1>
+                <img src={pokemon.sprites.front_default} alt="Pokemon front sprite" layout='fill' />
+            </div>
+            <div className = "pokemonDescription">
+            <h3>Pokemon's Info:</h3>
+            <br />
+            
+            <p>
+            </p>
+            <br />
 
-        <h3>Pokemon's Info:</h3>
-        <br />
-        
-        <p>
-            <b>Type: </b> 
-            {pokemon.types.map( (type, index) => {
-                return (<span>{index ? ', ' : ''} {type.type.name}</span>)
-            })}
-        </p>
-        <br />
+            <p>
+                <b>Abilities:</b>
+                <ul>
+                {pokemon.abilities.map( ability => {
+                    return (<li>{ability.ability.name}</li>)
+                })}
+                </ul>
+            </p>
+            <br />
 
-        <p>
-            <b>Abilities:</b>
-            <ul>
-            {pokemon.abilities.map( ability => {
-                return (<li>{ability.ability.name}</li>)
-            })}
-            </ul>
-        </p>
-        <br />
+            <p>
+                <b>Stats: </b>
+                <ul>
+                {pokemon.stats.map( stat => {
+                    return (<li>{stat.stat.name}: {stat.base_stat}</li>)
+                })}
+                </ul>
+            </p>
+            <br />
 
-        <p>
-            <b>Stats: </b>
-            <ul>
-            {pokemon.stats.map( stat => {
-                return (<li>{stat.stat.name}: {stat.base_stat}</li>)
-            })}
-            </ul>
-        </p>
-        <br />
-
-        <p>
-            <b>Games: </b>
-            {pokemon.game_indices.map( (game, index) => {
-                return (<span>{index ? ', ' : ''} {game.version.name}</span>)
-            })}
-        </p>
-
-        
-
-        </div>
+            <p>
+                <b>Games: </b>
+                {pokemon.game_indices.map( (game, index) => {
+                    return (<span>{index ? ', ' : ''} {game.version.name}</span>)
+                })}
+            </p>
+            </div>
+        </main>
     )
 };
 
