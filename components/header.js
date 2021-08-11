ab2e036e4986c6951d926928cc9abc87ba925ad2
Arthur Grillo <arthur.grillo@usp.br>
2021-08-11 14:28:24 -0300

    Adding the home Link to the header component

--- components/header.js
+++ components/header.js
@@ -1,19 +1,23 @@
 import classe from '../styles/Header.module.css'
+import Link from 'next/link'
 
 export default function Header(){
     return(
+      <Link href="/">
+      <a>
       <header className={classe.header}>
         <img
           className={classe.iconeImg}
           src="Icone.png"
           alt="Icone"
         />
-
         <img
           className={classe.pokedexImg}
           src="PokéDex.png"
           alt="PokéDex"
         />
       </header>
+      </a>
+      </Link>
     )
 }
